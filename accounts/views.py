17917552e5bda365ba88c392cb0f7f63e98bcb0b
rnaji0 <najirami680@gmail.com>
2026-03-03 13:39:24 -0500

    User Story 5 - I added the ability to see most purchased movie and the most reviewed movie. + extended functionality of seeing rankings within

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.admin.views.decorators import staff_member_required
-from django.db.models import Sum
+from django.db.models import Sum, Count 
+from cart.models import Item
+from movies.models import Movie, Review
 
 @login_required
 def logout(request):
@@ -65,6 +67,26 @@ def admin_dashboard(request):
         .annotate(total_movies=Sum('order__item__quantity'))
         .order_by('-total_movies')
     )
+
+    most_purchased = (
+        Movie.objects.annotate(total_purchased = Sum('item__quantity'))
+        .filter(total_purchased__isnull=False)
+        .order_by('-total_purchased')
+    )
+
+    most_reviewed = (
+        Movie.objects.annotate(total_reviews= Count('review'))
+        .filter(total_reviews__gt=0)
+        .order_by('-total_reviews')
+    )
+
+    template_data['most_purchased'] = most_purchased
+    template_data['most_reviewed'] =most_reviewed
+
     template_data['users'] = users_with_purchases
     return render(request, 'accounts/admin_dashboard.html',
         {'template_data': template_data})
+
+
+
+
